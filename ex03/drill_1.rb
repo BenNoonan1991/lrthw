4eae97407e198d3ee925fb7b6a4aafdd627fccce
BenNoonan1991 <ben.noonan2@gmail.com>
2017-03-24 19:43:00 +0000

    Add ex03 and drills

---
+++ ex03/drill_1.rb
@@ -0,0 +1,24 @@
+# Purints string to screen.
+puts "I will now count my chickens:"
+#Calcluates no of hens/roosters and prints to screen.
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+#Prints to screen
+puts "Now I will count the eggs:"
+#Makes calculation.
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# Prints question to screen.
+puts "Is it true that 3 + 2 < 5 - 7?"
+# Puts false to screen.
+puts 3 + 2 < 5 - 7
+#Prints the question then the answer to the screen.
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+# Prints the question then answer to screen.
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
---
+++ ex03/drill_3.rb
@@ -0,0 +1,5 @@
+def seconds_in_year
+  (60 * 60) * 24 * 365
+end
+
+puts seconds_in_year
