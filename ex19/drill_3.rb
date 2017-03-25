a8bf1dd8b59cfa1984bcb38b1e842d9e936cae41
BenNoonan1991 <ben.noonan2@gmail.com>
2017-03-25 18:29:36 +0000

    Add ex19 drills

---
+++ ex19/drill_3.rb
@@ -0,0 +1,14 @@
+def multiply(x, y)
+  puts x * y
+end
+
+multiply(7, 3)
+multiply(4, 3)
+multiply(7, 2)
+multiply(7, 1)
+multiply(5, 5)
+multiply(3, 9)
+multiply(9, 8)
+multiply(4, 3)
+multiply(5, 2)
+multiply(9, 3)
