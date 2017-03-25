97246d0c67e7f07ba4fb69b06155136253ded9df
BenNoonan1991 <ben.noonan2@gmail.com>
2017-03-25 16:53:01 +0000

    Add ex05 and drills

---
+++ ex05/dill_2.rb
@@ -0,0 +1,23 @@
+name = 'Zed A. Shaw'
+age = 35
+height = 74
+weight = 180
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+
+height_in_cm = height * 2.54
+weight_in_kg = weight * 0.453592
+
+puts "My height in cm is #{height_in_cm}."
+puts "My weight in kg is #{weight_in_kg}."
