7bfb4c2cd00f2aecc1f4b8d36b626753c136d379
BenNoonan1991 <ben.noonan2@gmail.com>
2017-03-26 13:58:41 +0100

    Add ex34 and drills

---
+++ ex34/drill_2.rb
@@ -0,0 +1,9 @@
+players = ["Sanchez", "Ozil", "Koscielny", "Cech", "Giroud"]
+# The first player is at 0 and is Sanchez.
+# The second player is at 1 and is Ozil.
+# The third player is at 2 and is Koscielny.
+# The fourth player is at 3 and is Cech.
+# The fifth player is at 4 and is Giroud.
+
+puts "#{players[2]} is the third player."
+puts players[4]
---
+++ ex34/ex34.rb
@@ -0,0 +1,13 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+# The first animal is at 0 and is a bear.
+# The second animal is at 1 and is a ruby.
+# The third animal is at 2 and is a peacock.
+# The fourth animal is at 3 and is a kangaroo.
+# The fifth animal is at 4 and is a whale.
+# The sixth animal is at 5 and is a platypus.
+
+# 1 - There is no year 0.
+puts animals[0]
+
+# Drill 1 - It's 2010 instead of 2009 because there is no year 0,
+# it goes from 1BC to 1AD.
